a624a65f3ac5dca3d804e6f6b4f02b3d87bde796
Anna Skuratova <anna@molnix.com>
2023-01-09 19:43:39 +0200

    wip/font

--- src/components/TipCalculator.vue
+++ src/components/TipCalculator.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="grid rounded-lg bg-white shadow-2xl h-full w-full">
-    <div class="tip-amount grid grid-cols-2 content-center h-full w-full pt-2">
+    <div
+      class="tip-amount grid grid-cols-2 content-center h-full w-full pt-2 border-b-2 border-gray-100"
+    >
       <div
         class="label font-bold text-right flex items-center justify-end pr-6"
       >
@@ -23,8 +25,8 @@
       </div>
     </div>
 
-    <div class="input-fields grid grid-cols-2 bg-gray-100 pt-8">
-      <div class="bill-amount">
+    <div class="input-fields grid grid-cols-2 bg-gray-100">
+      <div class="bill-amount pt-8 border-r-2 border-gray-200">
         <div class="field flex items-baseline pl-6">
           <DollarIcon class="h-[25px] w-[25px]" />
           <input
@@ -37,7 +39,7 @@
         </div>
         <div class="label font-bold text-center pt-2">Bill Amount</div>
       </div>
-      <div class="number-of-people">
+      <div class="number-of-people pt-8">
         <div class="field flex items-baseline pl-6">
           <PeopleIcon class="h-[25px] w-[25px]" />
           <input
@@ -55,13 +57,19 @@
     <div class="tip-percentages grid grid-cols-4 bg-gray-200">
       <div class="flex items-center justify-center">
         <input type="radio" name="tip" value="5%" id="five-percent" />
-        <label for="five-percent" class="rounded-lg px-8 py-2 bg-white">
+        <label
+          for="five-percent"
+          class="tip-percentages-btn rounded-lg px-8 py-2 bg-white"
+        >
           5%
         </label>
       </div>
       <div class="flex items-center justify-center">
         <input type="radio" name="tip" value="10%" id="ten-percent" />
-        <label for="ten-percent" class="rounded-lg px-8 py-2 bg-white">
+        <label
+          for="ten-percent"
+          class="tip-percentages-btn rounded-lg px-8 py-2 bg-white"
+        >
           10%
         </label>
       </div>
@@ -73,13 +81,19 @@
           value="15%"
           id="fifteen-percent"
         />
-        <label for="fifteen-percent" class="rounded-lg px-8 py-2 bg-white">
+        <label
+          for="fifteen-percent"
+          class="tip-percentages-btn rounded-lg px-8 py-2 bg-white"
+        >
           15%
         </label>
       </div>
       <div class="flex items-center justify-center">
         <input type="radio" name="tip" value="20%" id="twenty-percent" />
-        <label for="twenty-percent" class="rounded-lg px-8 py-2 bg-white">
+        <label
+          for="twenty-percent"
+          class="tip-percentages-btn rounded-lg px-8 py-2 bg-white"
+        >
           20%
         </label>
       </div>
@@ -88,7 +102,7 @@
     <div class="button-wrapper bg-gray-100 flex items-center justify-end pr-7">
       <button
         id="calculate"
-        class="bg-orange-600 rounded-lg px-4 py-2 text-white"
+        class="bg-orange-600 rounded-lg px-4 py-2 text-white hover:bg-orange-700"
       >
         Calculate
       </button>
